e1ba9fb9afa8d780430713c6e069e11a92a06c26
Olga Lyubinets <olga.lyubinets@olgas-mbp.wkstn.qmul.ac.uk>
2019-02-18 15:50:09 +0000

    Added slider that shows forecast for next 23 hours

--- src/components/GeneralData.jsx
+++ src/components/GeneralData.jsx
@@ -1,19 +1,46 @@
 import React, { Component } from "react";
 import { createStyles, withStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/lab/Slider";
 
 class GeneralData extends Component {
+  state = {
+    sliderValue: 0
+  };
+
+  handleChange = (event, sliderValue) => {
+    this.setState({ sliderValue });
+  };
+
   render() {
-    const { classes, currentWeather, isLoaded } = this.props;
+    const { classes, currentWeather, forecastWeather, isLoaded } = this.props;
+    const { sliderValue } = this.state;
     return (
       <>
+        <div className={classes.sliderContainer}>
+          <Slider
+            classes={{ container: classes.slider }}
+            max="22"
+            value={sliderValue}
+            aria-labelledby="label"
+            onChange={this.handleChange}
+          />
+        </div>
         <h1 className={classes.container}>
-          {isLoaded && currentWeather.windMPH}mph
+          {sliderValue === 0
+            ? isLoaded && currentWeather.windKPH + " KPH"
+            : isLoaded &&
+              forecastWeather[Math.floor(sliderValue)].windSpeedKPH + " KPH"}
         </h1>
         <h1 className={classes.container}>
-          {isLoaded && currentWeather.tempC}°C
+          {sliderValue === 0
+            ? isLoaded && currentWeather.tempC + " °C"
+            : isLoaded &&
+              forecastWeather[Math.floor(sliderValue)].tempC + " °C"}
         </h1>
         <h1 className={classes.container}>
-          {isLoaded && currentWeather.weatherShort}
+          {sliderValue === 0
+            ? isLoaded && currentWeather.weatherShort
+            : isLoaded && forecastWeather[Math.floor(sliderValue)].weather}
         </h1>
       </>
     );
@@ -33,6 +60,11 @@ const styles = createStyles({
     marginBottom: "7%",
     marginLeft: "10%",
     top: "60%"
+  },
+  sliderContainer: {
+    marginLeft: "10%",
+    marginRight: "10%",
+    marginTop: "20%"
   }
 });
 
